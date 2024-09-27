0912a86382294e1836754344390b63cdbb15daa6
Obisanya1 <ifeoluwadaniel25@gmail.com>
2024-09-27 22:14:47 +0100

    mosh-typescript learn

---
+++ dist/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let numbers = [];
+let user = [20, 'Age', true];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+user.concat();
+console.log(user);
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+let sales:number = 123_456_789;
+let course:string = 'TypeScript';
+let is_published:boolean = true;
+
+let numbers: number[] = [];
+let user: [number, string, boolean] = [20, 'Age', true]
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+enum Size {Small = 1, Medium, Large }
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+user.concat()
+console.log(user)
